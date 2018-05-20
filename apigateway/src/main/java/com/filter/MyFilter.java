971d33f6bbca85cd655beb2e7244cbd4edfb970f
dddkl007 <691096897@qq.com>
2018-05-20 19:46:24 +0800

    api网关

---
+++ apigateway/src/main/java/com/filter/MyFilter.java
@@ -0,0 +1,38 @@
+package com.filter;
+
+import com.netflix.client.http.HttpRequest;
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+
+import javax.servlet.http.HttpServletRequest;
+
+public class MyFilter extends ZuulFilter {
+    @Override
+    public String filterType() {
+        return "pre";
+    }
+
+    @Override
+    public int filterOrder() {
+        return 0;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() {
+        RequestContext ctx = RequestContext.getCurrentContext();
+        HttpServletRequest httpServletRequest = ctx.getRequest();
+        String login = httpServletRequest.getParameter("login");
+        if(login == null){
+            ctx.setSendZuulResponse(false);
+            ctx.setResponseStatusCode(401);
+            ctx.addZuulResponseHeader("content-type","text/html;charset=utf-8");
+            ctx.setResponseBody("非法访问");
+        }
+        return null;
+    }
+}
